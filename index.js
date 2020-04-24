141b9020e44ee4682a3633b415258ddc956c8e9f
Gustavo <gustavohstorti@gmail.com>
2020-04-24 02:02:00 -0300

    add heroku files

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const app = express();   // create express app      
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000
+const mysql = require('mysql');
 
-// create express app
-const app = express();
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -12,10 +13,35 @@ app.use(bodyParser.json())
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to Epidemics API. For information on how to use, visit: https://github.com/gsalibi/epidemics-api"});
+    res.json({ "message": "Welcome to Epidemics API. For information on how to use, visit: https://github.com/gsalibi/epidemics-api" });
+});
+
+
+// define another route
+app.get('/test/:id?', (req, res) => {
+    let filter = '';
+    if(req.params.id) filter = ' WHERE id=' + parseInt(req.params.id);
+    execSQLQuery('SELECT * FROM TestTable' + filter, res);
 });
 
 // listen for requests
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
-});
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+function execSQLQuery(sqlQry, res){
+    const connection = mysql.createConnection({
+      host     : process.env.HOST,
+      port     : process.env.HOST_PORT,
+      user     : process.env.USER,
+      password : process.env.PASS,
+      database : process.env.DB
+    });
+  
+    connection.query(sqlQry, function(error, results, fields){
+        if(error) 
+          res.json(error);
+        else
+          res.json(results);
+        connection.end();
+        console.log('Resultado exibido!');
+    });
+  }
